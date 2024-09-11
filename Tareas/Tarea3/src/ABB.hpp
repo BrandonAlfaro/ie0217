701c325b8afb3c8a333641c9882c8fbb572af6e1
Brandon Alfaro H. <brandonandreyalfaro@gmail.com>
2024-09-11 11:23:46 -0600

    ABB: se modifica el archivo 'ABB.hpp'.
    Signed-off-by: Brandon Alfaro H. <brandonandreyalfaro@gmail.com>

--- Tareas/Tarea3/src/ABB.hpp
+++ Tareas/Tarea3/src/ABB.hpp
@@ -7,25 +7,25 @@
 using namespace std;
 
 class ArbolBinarioBusqueda {
-private:
-    NodoArbol* raiz; // Puntero al nodo raiz del arbol
+    private:
+        NodoArbol* raiz; // Puntero al nodo raiz del arbol
 
-    void insertarNodo(NodoArbol*& nodo, int valor);
-    void inOrden(NodoArbol* nodo) const;
-    bool buscarNodo(NodoArbol* nodo, int valor) const;
-    int altura(NodoArbol* nodo) const;
-    bool estaBalanceado(NodoArbol* nodo) const;
-    NodoArbol* eliminarNodo(NodoArbol* nodo, int valor);
-    NodoArbol* encontrarMinimo(NodoArbol* nodo);
+        void insertarNodo(NodoArbol*& nodo, int valor);
+        void inOrden(NodoArbol* nodo) const;
+        bool buscarNodo(NodoArbol* nodo, int valor) const;
+        int altura(NodoArbol* nodo) const;
+        bool estaBalanceado(NodoArbol* nodo) const;
+        NodoArbol* eliminarNodo(NodoArbol* nodo, int valor);
+        NodoArbol* encontrarMinimo(NodoArbol* nodo);
 
-public:
-    ArbolBinarioBusqueda(); // Constructor que inicializa el árbol vacío
-    void insertar(int valor); // Inserta un valor en el árbol
-    void inOrden() const; // Realiza un recorrido en inorden
-    bool buscar(int valor) const; // Busca un valor en el árbol
-    int altura() const; // Devuelve la altura del árbol
-    bool estaBalanceado() const; // Verifica si el árbol está balanceado
-    void eliminar(int valor); // Elimina un valor del árbol
-};
+    public:
+        ArbolBinarioBusqueda(); // Constructor que inicializa el árbol vacío
+        void insertar(int valor); // Inserta un valor en el árbol
+        void inOrden() const; // Realiza un recorrido en inorden
+        bool buscar(int valor) const; // Busca un valor en el árbol
+        int altura() const; // Devuelve la altura del árbol
+        bool estaBalanceado() const; // Verifica si el árbol está balanceado
+        void eliminar(int valor); // Elimina un valor del árbol
+    };
 
 #endif // ABB_HPP
