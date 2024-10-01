d40443032da01c4024f0f974914fa5733bf06a05
Brandon Alfaro H. <brandonandreyalfaro@gmail.com>
2024-10-01 10:41:47 -0600

    Benchmarks en algoritmos de ordenamiento: Se agrega el header file de datos aleatorios.
    Signed-off-by: Brandon Alfaro H. <brandonandreyalfaro@gmail.com>

---
+++ Tareas/Tarea4/src/random_data.hpp
@@ -0,0 +1,11 @@
+#ifndef RANDOM_DATA_HPP
+#define RANDOM_DATA_HPP
+
+#include <vector>
+
+using namespace std;
+
+// Función para generar 'n' datos aleatorios.
+vector<int> randomData(size_t n);
+
+#endif // RANDOM_DATA_HPP
